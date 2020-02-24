4a58ca9e64f5ab5c33106f44e85c2debe59a3097
Jiwoo Jang <jiwooo.jang@gmail.com>
2020-02-24 00:12:52 -0500

    Finish boxing game, add scoring and physics

---
+++ Assets/Scripts/ScoreDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDetector : MonoBehaviour
+{
+    public bool shouldDetectLeft;
+    public GameObject debug;
+
+    private void OnTriggerEnter(Collider other) {
+        if (shouldDetectLeft) {
+            if (other.gameObject.tag == "LeftHandCollider") {
+                ScoreManager.instance.IncreaseScore();
+            }
+        } else {
+            if (other.gameObject.tag == "RightHandCollider") {
+                ScoreManager.instance.IncreaseScore();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    private int score = 0;
+
+    [SerializeField]
+    private Text scoreText;
+
+    public void IncreaseScore() {
+        score++;
+        Debug.Log("Incrementing Score");
+        scoreText.text = "SCORE: " + score.ToString();
+    }
+
+    public void WriteGameObjectName(string name) {
+        scoreText.text = name;
+    }
+
+    void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Debug.LogError("More than once instance of the ScoreManager Singleton. Deleting the old instance.");
+            DestroyImmediate(instance);
+
+            instance = this;
+        }
+    }
+}
